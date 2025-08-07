f2aff030df0d6ecc19ba8d930994a1505af52505
twilinger <rafaelasdeveloper@gmail.com>
2025-08-08 01:12:51 +0300

    db imported

---
+++ data/import_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from db_config import DATABASE_CONFIG
+
+
+engine = create_engine(f"postgresql+psycopg2://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}:{DATABASE_CONFIG['port']}/{DATABASE_CONFIG['database']}")
+file_path = r'C:\Users\rafae\OneDrive\Desktop\projects\Client_Churn_analyzer\data\Telco-Customer-Churn.csv'
+df = pd.read_csv(file_path)
+
+df.columns = [col.lower() for col in df.columns] 
+df['totalcharges'] = pd.to_numeric(df['totalcharges'], errors='coerce').fillna(0)
+
+df.to_sql(
+    name='customers',
+    con=engine,
+    if_exists='replace',  
+    index=False,
+    method='multi'
+)
+
+print(f"Успешно импортировано {len(df)} записей в таблицу customers")
--- data/test_db.py
+++ data/test_db.py
@@ -1,20 +1,21 @@
 from sqlalchemy import create_engine, text
-from db_config import DATABASE_CONFIG  # Импорт конфига
+from db_config import DATABASE_CONFIG
+import pandas as pd
 
 def test_connection():
     try:
-        # Формируем строку подключения (пример для PostgreSQL)
         connection_str = f"postgresql://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}:{DATABASE_CONFIG['port']}/{DATABASE_CONFIG['database']}"
         
-        # Подключаемся к БД
         engine = create_engine(connection_str)
         with engine.connect() as conn:
-            # Выполняем тестовый запрос (например, проверяем список таблиц)
             result = conn.execute(text("SELECT table_name FROM information_schema.tables WHERE table_schema='public'"))
             tables = [row[0] for row in result]
             
             print("✅ Подключение успешно!")
             print(f"Найдены таблицы: {tables}")
+            df = pd.read_sql("SELECT * FROM customers LIMIT 5", engine)
+            print("Первые 5 записей:")
+            print(df)
         return True
     except Exception as e:
         print(f"❌ Ошибка подключения: {e}")
@@ -22,3 +23,4 @@ def test_connection():
 
 if __name__ == "__main__":
     test_connection()
+
